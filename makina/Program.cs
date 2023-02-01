b816992a5bd85236e823d7bbbfcd40bdb657cfd6
ankitabylle <ankit@abylle.com>
2023-02-01 16:14:25 +0530

    ttt

--- makina/Program.cs
+++ makina/Program.cs
@@ -52,7 +52,7 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
     endpoints.MapGet("/", async context =>
     {
-        await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda");
+        await context.Response.WriteAsync("Welcome to running ASP.NET Core on AWS Lambda1");
     });
 });
 app.Run();
